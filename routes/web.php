c476a7a0f0b4736d1536a0bdcabfe31def6b8718
aaron <616915054@qq.com>
2024-08-05 15:44:04 +0800

    add web file

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/index.html');
+//    return view('welcome');
+//    return file_get_contents(public_path('index.html'));
+//    return view(public_path('index.html'));
 });
